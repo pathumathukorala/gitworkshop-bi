537bc7c24463334a2950edc9b252bea68245665d
Pathum Athukorala <pathum.athukorala@ebuilder.com>
2016-05-25 10:42:12 +0530

    add function two

--- src/com/ebuilder/gitworkshop/bi/MainRunner.java
+++ src/com/ebuilder/gitworkshop/bi/MainRunner.java
@@ -5,6 +5,7 @@ public class MainRunner {
 	public static void main(String[] args) {
 		
 		System.out.println("Initial function goes here");
+		System.out.print("function two goes in here");
 
 	}
 
